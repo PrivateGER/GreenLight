7776b97f66d8c2beb460e90141b1b12783309f76
PerhapsSomeone <ma2003br@gmail.com>
2020-05-06 02:36:40 +0200

    Add HTTP support to httpGet() and add user defined functions

--- greenlight.js
+++ greenlight.js
@@ -1,12 +1,19 @@
+let colors = require("./lib/colors");
+console.log(colors.green + "⬤ GreenLight v0.0.1" + colors.reset)
+
 let fs = require("fs");
 let lexer = require("./lib/lexer");
 let transpiler = require("./lib/transpiler");
-let colors = require("./lib/colors");
 
 let code = "";
 
-if(process.argv[2] != null) {
-	for(let i = 2; process.argv[i] != null; i++) {
+if(process.argv[2] == null || !( process.argv[2] === "e" || process.argv[2] === "c" )) {
+	console.log(colors.red + "[ERR] Please provide either e or c as second argument!" + colors.reset);
+	process.exit(1);
+}
+
+if(process.argv[3] != null) {
+	for(let i = 3; process.argv[i] != null; i++) {
 		if(!fs.existsSync(process.argv[i])) {
 			console.log(colors.red + "[ERR] " + process.argv[i] + " doesn't exist!" + colors.reset);
 			process.exit(1);
@@ -18,8 +25,6 @@ if(process.argv[2] != null) {
 	process.exit(1);
 }
 
-console.log(colors.green + "⬤ GreenLight v0.0.1" + colors.reset)
-
 let lexerResults = lexer.execute(code)
 
 //console.log(lexerResults);
@@ -28,5 +33,8 @@ lexerResults.forEach(token => {
 	transpiler.transpile(token);
 })
 
-//console.log(transpiler.getCode());
-eval(transpiler.getCode());
+if(process.argv[2] === "c") {
+	console.log(transpiler.getCode());
+} else {
+	eval(transpiler.getCode());
+}
